b0d5c76dfb653904983f31b47bd1c2df635b9190
Bhaskar <bhaskar@agiliq.com>
2013-04-17 12:03:52 +0530

    Added Test: TestNumber - test_get_numbers

--- tests.py
+++ tests.py
@@ -280,6 +280,22 @@ class TestRecording(unittest.TestCase):
             self.assertTrue(key in json_response)
 
 
+class TestNumber(unittest.TestCase):
+    def setUp(self):
+        auth_id = AUTH_ID
+        auth_token = AUTH_TOKEN
+        self.client = plivo.RestAPI(auth_id, auth_token)
+
+    
+    def test_get_numbers(self):
+        response = self.client.get_numbers()
+        self.assertEqual(200, response[0])
+        valid_keys = ['meta', 'objects', 'api_id']
+        json_response = response[1]
+        for key in valid_keys:
+            self.assertTrue(key in json_response)
+
+
 
 if __name__ == "__main__":
     unittest.main()
